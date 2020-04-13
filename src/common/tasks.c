a1b0bf37ffa0cbdf1122d4a88133c326beb43d44
Mirko Myllykoski <mirkom@cs.umu.se>
2020-04-13 15:55:10 +0200

    Bulk update copyright years

--- src/common/tasks.c
+++ src/common/tasks.c
@@ -8,7 +8,7 @@
 ///
 /// @section LICENSE
 ///
-/// Copyright (c) 2019, Umeå Universitet
+/// Copyright (c) 2019-2020, Umeå Universitet
 ///
 /// Redistribution and use in source and binary forms, with or without
 /// modification, are permitted provided that the following conditions are met:
